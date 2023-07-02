ab1177fa9ed94b4465f97e7ea19075b5c941fc3f
yike5460 <yike5460@163.com>
2023-07-02 16:34:49 +0800

    fix: remove security leakage from git secret result

--- deploy/bin/deploy.js
+++ deploy/bin/deploy.js
@@ -17,7 +17,6 @@ new DeployStack(app, 'QAChatDeployStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '946277762357', region: 'us-west-2' },
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
