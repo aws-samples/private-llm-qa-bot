e9d836ca81b096b11cc1d501b8245977bcf97760
Yuanbo Li <ybalbert@amazon.com>
2023-08-31 19:23:55 +0800

    1. support cdk deploy sagemaker domain and setup role 2. update CommonCrawl notebook

--- deploy/bin/deploy.js
+++ deploy/bin/deploy.js
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
-import cdk  from 'aws-cdk-lib';
+import cdk from 'aws-cdk-lib';
 import { DeployStack } from '../lib/deploy-stack.js';
+import { SagemakerDomainStack } from '../lib/sagemaker.js';
 import * as dotenv from 'dotenv' 
 dotenv.config()
 
 console.log(process.env.CDK_DEFAULT_ACCOUNT,process.env.CDK_DEFAULT_REGION);
 const app = new cdk.App();
 new DeployStack(app, 'QAChatDeployStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
+
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+new SagemakerDomainStack(app, 'SagemakerDeployStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ deploy/lib/sagemaker.js
@@ -0,0 +1,64 @@
+import { Stack, CfnOutput } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as sagemaker from "aws-cdk-lib/aws-sagemaker"
+
+export class SagemakerDomainStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const region = props.env.region;
+    const account_id = Stack.of(this).account;
+    
+    const defaultVpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', { isDefault: true });
+    const subnetIds = [];
+    
+    defaultVpc.publicSubnets.forEach((subnet, index) => {
+      subnetIds.push(subnet.subnetId);
+    });
+    
+    const sagemakerExecutionRole = new iam.Role(this, 'sagemaker-execution-role', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('glue.amazonaws.com'), new iam.ServicePrincipal('sagemaker.amazonaws.com')),
+      inlinePolicies: {
+        "chatbot-cc-policy": new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ["iam:GetRole", "iam:PassRole", "sts:GetCallerIdentity"],
+              resources: ["*"],
+            }),
+          ],
+        }),
+      },
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSageMakerFullAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AwsGlueSessionUserRestrictedServiceRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
+      ],
+    });
+
+    // Create the SageMaker Domain
+    const domain = new sagemaker.CfnDomain(this, 'SageMakerDomain', {
+      domainName: 'chatbot-sagemaker-domain', // Replace with your preferred domain name
+      authMode: 'IAM',
+      vpcId: defaultVpc.vpcId, // Replace with your VPC ID
+      subnetIds: subnetIds, // Replace with your subnet IDs
+      defaultUserSettings: {
+        executionRole: sagemakerExecutionRole.roleArn,
+      },
+    });
+
+    // SageMaker User Profile
+    const userProfile = new sagemaker.CfnUserProfile(this, 'LLMUser', {
+      domainId: domain.attrDomainId,
+      userProfileName: 'llm-user-1',
+      userSettings: {
+        executionRole: sagemakerExecutionRole.roleArn,
+      },
+    })
+    
+    new CfnOutput(this,'SagemakerARN',{value:sagemakerExecutionRole.roleArn});
+  }
+}
