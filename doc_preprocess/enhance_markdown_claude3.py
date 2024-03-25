adeac4fdc5578825ca2767408e8d0e2af817437d
Yuanbo Li <ybalbert@amazon.com>
2024-03-25 17:12:55 +0800

    update QA generator PE

--- doc_preprocess/enhance_markdown_claude3.py
+++ doc_preprocess/enhance_markdown_claude3.py
@@ -27,35 +27,43 @@ def construct_gen_summary_prompt(content):
 
     return body
 
-def construct_QA_list_prompt(content):
-    examples = """{"question" : [ "question1", "question2", ...]}"""
+def construct_QA_list_prompt(content, NUMBER=5):
+    PERSPECTIVE = "FAQ Organizer"
+    examples = """[{"Question": "Q1", "Answer": "A1"}, {"Question": "Q2", "Answer": "A2"}, ...]"""
 
-    prompt = f"""You are interviewing a candidate for a customer service at a well-established game company. 
+    prompt = f"""Utilize Natural Language Processing techniques and Generative AI to create new Question/Answer pair textual training data for OpenAI LLMs by drawing inspiration from the given seed content: <content>{content}</content> 
 
-    The ideal candidate should have experience to answer players' questions. Below is the given document.
+Here are the steps to follow:
 
-    <document>
-    {content}
-    </document>
+1. Examine the provided seed content to identify significant and important topics, entities, relationships, and themes. You should use each important topic, entity, relationship, and theme you recognize. You can employ methods such as named entity recognition, content summarization, keyword/keyphrase extraction, and semantic analysis to comprehend the content deeply.
 
-    Please give a series of questions based on the document, for checking the candidates's mastery。Avoid yes/no questions or those with obvious answers. 
+2. Based on the analysis conducted in the first step, employ a generative language model to generate fresh, new synthetic text samples. These samples should cover the same topic, entities, relationships, and themes present in the seed data. Aim to generate {NUMBER} high-quality variations that accurately explore different Question and Answer possibilities within the data space.
 
-    Please put your output in Json format, for example:
+3. Ensure that the generated synthetic samples exhibit language diversity. Vary elements like wording, sentence structure, tone, and complexity while retaining the core concepts. The objective is to produce diverse, representative data rather than repetitive instances.
 
-    {examples}
-    """
+4. Format and deliver the generated synthetic samples in a structured Pandas Dataframe suitable for training and machine learning purposes.
+
+5. The desired output length is roughly equivalent to the length of the seed content.
+
+Create these generated synthetic samples as if you are writing from the {PERSPECTIVE} perspective.
+
+Do not include any commentary or extraneous casualties.
+
+Only output the resulting dataframe in the format of this example:  <output>{examples}</output>
+
+"""
 
     messages = [ 
         {"role":"user", "content" : prompt },
-        {"role":"assistant", "content": "{\"questions\" : ["}
+        {"role":"assistant", "content": "<output>"}
     ]
 
     input_body = {}
     input_body["anthropic_version"] = "bedrock-2023-05-31"
     input_body["messages"] = messages
     input_body["system"] = "You are a interviewer, your task is to generate a series of questions for the interview based. The questions should be asked frequently in realistic scenario， Especially in Game Customer Service."
-    input_body["max_tokens"] = 1024
-    input_body["stop_sequences"] = [']']
+    input_body["max_tokens"] = 2048
+    input_body["stop_sequences"] = ['</output>']
 
     body = json.dumps(input_body)
 
@@ -135,7 +143,8 @@ if __name__ == '__main__':
             enhanced_data.append([summary, content, 'Nochunk-Summary'])
 
             qa_list_str = call_bedrock_enhance(content, construct_QA_list_prompt)
-            qa_list = [ [q.replace('"','').replace('\n','').strip(), content, 'Nochunk-Question' ] for q in qa_list_str.split('",') ]
+            obj_list = json.loads(qa_list_str)
+            qa_list = [ [ item['Question'], content, 'Nochunk-Question' ] for item in obj_list ]
             enhanced_data.extend(qa_list)
 
             df = pd.DataFrame(enhanced_data, columns=['Question', 'Answer', 'doc_type'])
