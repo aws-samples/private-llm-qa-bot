d7a595f691f2f1a1e6ac2dedccc781613c7277cd
Yuanbo Li <ybalbert@amazon.com>
2024-04-18 08:01:05 +0800

    Fix China region deploy 3

--- code/generator/llm_manager.py
+++ code/generator/llm_manager.py
@@ -5,34 +5,41 @@ SMM_KEY_AVAIL_LLM_ENDPOINTS = 'avail_llm_endpoints'
 OTHER_ACCOUNT_LLM_ENDPOINTS = None
 
 def get_all_bedrock_llm():
-    bedrock = boto3.client(
-        service_name='bedrock',
-        region_name='us-west-2'
-    )
+    model_ids = []
+    try:
+        bedrock = boto3.client(
+            service_name='bedrock',
+            region_name='us-west-2'
+        )
 
-    bedrock.list_foundation_models()
+        bedrock.list_foundation_models()
 
-    response = bedrock.list_foundation_models(
-        byOutputModality='TEXT',
-        byInferenceType='ON_DEMAND'
-    )
-    model_ids = [ item['modelId'] for item in response['modelSummaries'] if not item['modelId'].startswith('mistral') ]
+        response = bedrock.list_foundation_models(
+            byOutputModality='TEXT',
+            byInferenceType='ON_DEMAND'
+        )
+        model_ids = [ item['modelId'] for item in response['modelSummaries'] if not item['modelId'].startswith('mistral') ]
+    except Exception as e:
+        print(f"{str(e)}")
+
     return model_ids
 
 def get_all_private_llm(other_account_list=OTHER_ACCOUNT_LLM_ENDPOINTS):
     ret = {}
-
-    # only get the llm endpoint from this account
-    ssm = boto3.client('ssm')
     try:
-        parameter = ssm.get_parameter(Name=SMM_KEY_AVAIL_LLM_ENDPOINTS, WithDecryption=False)
-        ret=json.loads(parameter['Parameter']['Value'])
-    except Exception as e:
-        print("There is no llm endpoint existed.")
+        # only get the llm endpoint from this account
+        ssm = boto3.client('ssm')
+        try:
+            parameter = ssm.get_parameter(Name=SMM_KEY_AVAIL_LLM_ENDPOINTS, WithDecryption=False)
+            ret=json.loads(parameter['Parameter']['Value'])
+        except Exception as e:
+            print("There is no llm endpoint existed.")
 
-    if type(other_account_list) == list:
-        # get all of llm endpoint from other account
-        pass
+        if type(other_account_list) == list:
+            # get all of llm endpoint from other account
+            pass
+    except Exception as e:
+        print(f"{str(e)}")
         
     return ret
 
